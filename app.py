a5650e5b4b036ff56e36559f6d0e52965ab504b6
Mr Coleman <mrc@iepgroup.es>
2023-10-02 19:46:08 +0200

    Add SQLite database

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, time
 import os
 
+# create a new Flask application instance
 app = Flask(__name__)
 
+# configure the application to use a SQLite database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///timetabling.db"
+
+# create a new SQLAlchemy database instance
+db = SQLAlchemy(app)
+
+# define a new database model for bookings
+class Bookings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    parent_name = db.Column(db.String(80), nullable=False)
+    student_name = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    time = db.Column(db.Time, nullable=False, default=time.min)
+    def __repr__(self):
+        return f"<Booking {self.id}>"
+
+# define a new Flask route for serving the index.html file
 @app.route("/")
 def index():
-    return send_from_directory(os.path.join(app.root_path, "static"), "index.html")
+    return render_template("index.html")
 
+# run the application if this file is executed directly
 if __name__ == "__main__":
     app.run(debug=True)
