cd30073b2a574436ec49669d2eea336376ccfcb2
TC <tom@cabranet.dev>
2023-10-15 18:52:24 +0200

    POST via an async function and add the data to the Booking table

--- app.py
+++ app.py
@@ -2,46 +2,18 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookingsdb.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Teacher(db.Model):
-    teacher_id = db.Column(db.Integer, primary_key=True)
-    teacher_first_name = db.Column(db.String(50), nullable=False)
-    teacher_surname = db.Column(db.String(50), nullable=False)
-    teacher_subject = db.Column(db.String(50), nullable=False)
-    bookings = db.relationship('ParentBooking', backref='teacher', lazy=True)
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    parent_name = db.Column(db.String(80))
+    student_name = db.Column(db.String(80))
+    teacher = db.Column(db.String(80))
+    date = db.Column(db.String(10))
+    time = db.Column(db.String(10))
 
-class Parent(db.Model):
-    parent_id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
-    parent_first_name = db.Column(db.String(50), nullable=False)
-    parent_surname = db.Column(db.String(50), nullable=False)
-    bookings = db.relationship('ParentBooking', backref='parent', lazy=True)
-
-class Student(db.Model):
-    student_id = db.Column(db.Integer, primary_key=True)
-    student_first_name = db.Column(db.String(50), nullable=False)
-    student_surname = db.Column(db.String(50), nullable=False)
-    class_id = db.Column(db.Integer, db.ForeignKey('class.class_id'), nullable=False)
-    parents = db.relationship('Parent', backref='student', lazy=True)
-    bookings = db.relationship('ParentBooking', backref='student', lazy=True)
-
-class Class(db.Model):
-    class_id = db.Column(db.Integer, primary_key=True)
-    class_number = db.Column(db.Integer, nullable=False)
-    class_name = db.Column(db.String(50), nullable=False)
-    class_floor = db.Column(db.Integer, nullable=False)
-    students = db.relationship('Student', backref='class_', lazy=True)
-
-class ParentBooking(db.Model):
-    booking_id = db.Column(db.Integer, primary_key=True)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.teacher_id'), nullable=False)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
-    parent_id = db.Column(db.Integer, db.ForeignKey('parent.parent_id'), nullable=False)
-    booking_date = db.Column(db.Date, nullable=False)
-    booking_time = db.Column(db.Time, nullable=False)
 
 @app.route("/")
 def index():
@@ -50,10 +22,40 @@ def index():
 @app.route('/bookappointment', methods=['GET', 'POST'])
 def book_appointment():
     if request.method == 'POST':
-        booking = request.get_json()
-        # Do something with booking (e.g., save to database)
-        print(booking)
-        return render_template('bookingform.html', booking=booking)
+        # Accessing form data
+        parent = request.form.get('parentName')
+        student = request.form.get('studentName')
+        teacher = request.form.get('teacher')
+        date = request.form.get('date')
+        time = request.form.get('time')
+
+        # Creating a new Booking object
+        new_booking = Booking(
+            parent_name=parent,
+            student_name=student,
+            teacher=teacher,
+            date=date,
+            time=time
+        )
+
+        # Adding the new booking to the database
+        db.session.add(new_booking)
+        db.session.commit()
+        
+        # Creating a dictionary to be JSONified
+        booking_data = {
+            'id': new_booking.id,
+            'parent': new_booking.parent_name,
+            'student': new_booking.student_name,
+            'teacher': new_booking.teacher,
+            'date': new_booking.date,
+            'time': new_booking.time
+        }
+        return jsonify(booking_data)
+
+    # Handling GET request
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/scripts/main-script.js
+++ static/scripts/main-script.js
@@ -1,50 +1,105 @@
-let bookings = [];
+// Enforcing strict mode to catch common coding mistakes
+'use strict';
 
-document
-  .getElementById('book-appointment')
-  .addEventListener('click', function () {
-    const parentName = document.getElementById('parent-name').value;
-    const studentName = document.getElementById('student-name').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
+// Get the button element for booking and store it in the variable 'bookingBtn'
+const bookingBtn = document.getElementById('book-appointment');
 
-    const newBooking = {
-      parentName,
-      studentName,
-      date,
-      time,
-    };
+// Asynchronous function to send the booking data to the server
+async function sendBooking(newBooking) {
+  try {
+    // Creating a FormData object to hold the booking data
+    const formData = new FormData();
 
-    bookings.push(newBooking);
-
-    document.getElementById('booking-details').textContent = JSON.stringify(
-      bookings,
-      null,
-      2
-    );
-
-    // Send POST request to Flask server
-    async function sendBooking(newBooking) {
-      try {
-        const response = await fetch('/bookappointment', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(newBooking),
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log('Success:', data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
+    // Looping through the properties of newBooking and appending them to formData
+    for (const key in newBooking) {
+      formData.append(key, newBooking[key]);
     }
 
-    // Reset the form
-    document.getElementById('booking-form').reset();
-  });
+    // Sending a POST request to the server with the booking data
+    const response = await fetch('/bookappointment', {
+      method: 'POST',
+      body: formData,
+    });
+
+    // Checking if the response status is not OK (200-299)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Parsing the JSON response from the server
+    const data = await response.json();
+
+    // Logging the server response to the console
+    console.log('Success:', data);
+
+    // Calling a function to display the booking confirmation, passing the parsed data
+    displayBookingConfirmation(data);
+  } catch (error) {
+    // Logging any errors to the console
+    console.error('Error:', error);
+    // Displaying the error message in the booking-details div
+    displayBookingError(error);
+  }
+}
+
+//
+function displayBookingError(error) {
+  const bookingDetailsDiv = document.getElementById('booking-details');
+
+  // Constructing an HTML string with the error message
+  const errorHtml = `
+      <h2>Booking Error</h2>
+      <p>Sorry, there was an error with your booking:</p>
+      <p class="error-message">${error.message}</p>
+  `;
+
+  // Inserting the HTML string into the bookingDetailsDiv, thereby updating the webpage
+  bookingDetailsDiv.innerHTML = errorHtml;
+}
+
+// Function to display the booking confirmation in the webpage
+function displayBookingConfirmation(booking) {
+  // Getting the div element to display the booking details
+  const bookingDetailsDiv = document.getElementById('booking-details');
+
+  // Constructing an HTML string with the booking details
+  const confirmationHtml = `
+      <h2>Booking Confirmation</h2>
+      <p>Thank you for booking an appointment. Here are the details:</p>
+      <ul>
+          <li><strong>Parent Name:</strong> ${booking.parent}</li>
+          <li><strong>Student Name:</strong> ${booking.student}</li>
+          <li><strong>Teacher:</strong> ${booking.teacher}</li>
+          <li><strong>Date:</strong> ${booking.date}</li>
+          <li><strong>Time:</strong> ${booking.time}</li>
+      </ul>
+  `;
+
+  // Inserting the HTML string into the bookingDetailsDiv, thereby updating the webpage
+  bookingDetailsDiv.innerHTML = confirmationHtml;
+}
+
+// Adding a click event listener to the booking button
+bookingBtn.addEventListener('click', function () {
+  // Getting the input values from the form and storing them in variables
+  const parentName = document.getElementById('parent-name').value;
+  const studentName = document.getElementById('student-name').value;
+  const teacher = document.getElementById('teacher').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+
+  // Creating a new booking object with the input values
+  const newBooking = {
+    parentName,
+    studentName,
+    teacher,
+    date,
+    time,
+  };
+
+  // Calling the sendBooking function, passing the new booking data
+  sendBooking(newBooking);
+
+  // Resetting the form to clear the input fields
+  document.getElementById('booking-form').reset();
+});
